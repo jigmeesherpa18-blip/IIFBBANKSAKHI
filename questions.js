9b58d30779dc3b93b637c7a1d1363e0fcf567777
jigmeesherpa18-blip <jigmeesherpa18@gmail.com>
2026-02-04 14:00:00 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,33 @@
+const questions = [
+  {
+    q: "At the time of opening an account, banks advise customers to indicate nominee. Which statement is correct?",
+    o: [
+      "Amount is transferred only after legal heir verification",
+      "Multiple nominees allowed in single account",
+      "Nomination can be made in favor of legal entities",
+      "Nominee gets payment from bank without becoming owner"
+    ],
+    a: 3
+  },
+  {
+    q: "What is the full form of KYC?",
+    o: [
+      "Know Your Customer",
+      "Keep Your Cash",
+      "Know Your Credit",
+      "Keep Your Customer"
+    ],
+    a: 0
+  },
+  {
+    q: "Which organization regulates banks in India?",
+    o: [
+      "SEBI",
+      "IRDAI",
+      "RBI",
+      "NABARD"
+    ],
+    a: 2
+  }
+];
 
